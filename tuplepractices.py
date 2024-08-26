3432b860ae684c2f186137c5ac3ca6a5cae4004e
Ashish <ashishpal5393@gmail.com>
2024-08-26 08:04:34 +0530

    programs

---
+++ tuplepractices.py
@@ -0,0 +1,7 @@
+str_tuple = ("Apple","Orange","Mango")
+int_tuple = (15,25,36,84,59)
+float_tuple = (2.3,5.6,1.4,9.6)
+mixed_tuple = ("Easy",205,25.3)
+
+print("I like",str_tuple[0])
+print("I like",str_tuple[2])
